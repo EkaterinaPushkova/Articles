6f9073697b5e879bd91b7f002e89a3719371dca6
Екатерина Пушкова <pushkova_ea@mail.ru>
2022-09-19 18:23:15 +0300

    create component Article with button Delete

---
+++ src/components/Article.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { Dispatch } from 'redux'
+import { useDispatch } from 'react-redux'
+
+type Props = {
+    article: IArticle
+    deleteArticle: (article: IArticle) => void
+}
+
+export const Article: React.FC<Props> = ({article, deleteArticle}) => {
+    const dispatch: Dispatch<any> = useDispatch()
+
+    const delArticle = React.useCallback(
+        (article: IArticle) => 
+        dispatchEvent(deleteArticle(article)), 
+        [dispatch, deleteArticle]
+        )
+
+    return(
+        <div className='Article'>
+            <div>
+                <h1>{article.title}</h1>
+                <p>{article.content}</p>
+            </div>
+            <button onClick={() => delArticle(article)}>Delete</button>
+        </div>
+    )
+}
